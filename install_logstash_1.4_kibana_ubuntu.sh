505c26d776eb196479c6f1843f14a7d3748c40b8
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-05-20 10:47:44 -0400

    removing mailutils package

--- install_logstash_1.4_kibana_ubuntu.sh
+++ install_logstash_1.4_kibana_ubuntu.sh
@@ -31,7 +31,7 @@ sed -i -e 's|deb cdrom:|# deb cdrom:|' /etc/apt/sources.list
 apt-get -qq update
 
 # Install Pre-Reqs
-apt-get install -y --force-yes openjdk-7-jre-headless ruby ruby1.9.1-dev libcurl4-openssl-dev git apache2 curl mailutils
+apt-get install -y --force-yes openjdk-7-jre-headless ruby ruby1.9.1-dev libcurl4-openssl-dev git apache2 curl
 
 # Install Redis-Server
 apt-get -y install redis-server
