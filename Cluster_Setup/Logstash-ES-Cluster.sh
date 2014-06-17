dcd52059cfc290265019119dee8516808429d0c8
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-06-17 14:46:40 -0400

    updating IPTables filter

--- Cluster_Setup/Logstash-ES-Cluster.sh
+++ Cluster_Setup/Logstash-ES-Cluster.sh
@@ -342,7 +342,6 @@ filter {
         if "syslog" in [tags] {
                 if "IPTables" in [message] {
                         grok {
-                                break_on_match => false
                                 match => { "message" => "%{IPTABLES}" }
                                 patterns_dir => [ "/opt/logstash/patterns" ]
                         }
@@ -367,6 +366,17 @@ filter {
                         match => [
                                 "message", "IPTables-%{WORD:iptables_action}"
                         ]
+                }
+                grok {
+                        match => [
+                                "message", "PROTO=%{WORD:iptables_proto}"
+                        ]
+                }
+                mutate {
+                        remove_field => [ "proto" ]
+                }
+                mutate {
+                        rename => [ "iptables_proto", "proto" ]
                 }
         }
 }
