584e3630ff601b345b1ce6bd9108425364db9ff3
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-06-25 17:04:43 -0400

    Update Logstash-ES-Cluster-Master-data-node.sh

--- Cluster_Setup/Logstash-ES-Cluster-Master-data-node.sh
+++ Cluster_Setup/Logstash-ES-Cluster-Master-data-node.sh
@@ -62,9 +62,9 @@ echo "node.data: true" >> /etc/elasticsearch/elasticsearch.yml
 echo "index.number_of_shards: 5" >> /etc/elasticsearch/elasticsearch.yml
 echo "index.number_of_replicas: 1" >> /etc/elasticsearch/elasticsearch.yml
 echo "bootstrap.mlockall: true" >> /etc/elasticsearch/elasticsearch.yml
-echo "##### Change below instead of using multicast and update with your actual ES Master/Data nodenames #####" >> /etc/elasticsearch/elasticsearch.yml
+echo "##### Uncomment below instead of using multicast and update with your actual ES Master/Data nodenames #####" >> /etc/elasticsearch/elasticsearch.yml
 echo "#discovery.zen.ping.unicast.hosts: ["es-1", "es-2"]"
-echo "#discovery.zen.ping.multicast.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+echo "#discovery.zen.ping.multicast.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
 echo "#### Prevent split brain ES Cluster n/2+1 ####" >> /etc/elasticsearch/elasticsearch.yml
 echo "#discovery.zen.minimum_master_nodes: 1" >> /etc/elasticsearch/elasticsearch.yml
 
@@ -73,7 +73,7 @@ mv /etc/security/limits.conf /etc/security/limits.bak
 grep -Ev "# End of file" /etc/security/limits.bak > /etc/security/limits.conf
 echo "elasticsearch soft nofile 65536" >> /etc/security/limits.conf
 echo "elasticsearch hard nofile 65536" >> /etc/security/limits.conf
-echo "elasticsearch - memlock unlimited" >> /etc/security/limits.con
+echo "elasticsearch - memlock unlimited" >> /etc/security/limits.conf
 echo "# End of file" >> /etc/security/limits.conf
 
 # Modify elasticsearch service for ulimit -l unlimited to allow mlockall to work correctly
