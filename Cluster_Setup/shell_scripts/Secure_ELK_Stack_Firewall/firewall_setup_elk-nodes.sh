0a231ce59cd8bbaf20702df242dd4223d5d82452
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-06-30 11:41:37 -0400

    Update firewall_setup_elk-nodes.sh

--- Cluster_Setup/shell_scripts/Secure_ELK_Stack_Firewall/firewall_setup_elk-nodes.sh
+++ Cluster_Setup/shell_scripts/Secure_ELK_Stack_Firewall/firewall_setup_elk-nodes.sh
@@ -20,49 +20,49 @@ function pause(){
 for server in $(cat elk-nodes.txt); do
         # Flush existing rules and chains
         echo "Flushing existing Firewall rules on: " $server
-        ssh root@$server "iptables -P INPUT ACCEPT"
-        ssh root@$server "iptables -P FORWARD ACCEPT"
-        ssh root@$server "iptables -F"
-        ssh root@$server "iptables -X"
+        ssh elkadmin@$server "sudo iptables -P INPUT ACCEPT"
+        ssh elkadmin@$server "sudo iptables -P FORWARD ACCEPT"
+        ssh elkadmin@$server "sudo iptables -F"
+        ssh elkadmin@$server "sudo iptables -X"
 
         # Create new chains and setup logging
         echo "Setting up firewall chains and logging on" $server
-        ssh root@$server "iptables -N DEFAULT-Allowed"
-        ssh root@$server "iptables -N ELK-RELATED-Allowed"
-        ssh root@$server "iptables -N LOGGING-Allowed"
-        ssh root@$server "iptables -A LOGGING-Allowed -m limit --limit 2/min -j LOG --log-prefix "IPTables-Allowed: " --log-level 4"
-        ssh root@$server "iptables -A LOGGING-Allowed -j ACCEPT"
-        ssh root@$server "iptables -N LOGGING-Dropped"
-        ssh root@$server "iptables -A LOGGING-Dropped -m limit --limit 2/min -j LOG --log-prefix "IPTables-Dropped: " --log-level 4"
-        ssh root@$server "iptables -A LOGGING-Dropped -j REJECT --reject-with icmp-proto-unreachable"
+        ssh elkadmin@$server "sudo iptables -N DEFAULT-Allowed"
+        ssh elkadmin@$server "sudo iptables -N ELK-RELATED-Allowed"
+        ssh elkadmin@$server "sudo iptables -N LOGGING-Allowed"
+        ssh elkadmin@$server "sudo iptables -A LOGGING-Allowed -m limit --limit 2/min -j LOG --log-prefix "IPTables-Allowed: " --log-level 4"
+        ssh elkadmin@$server "sudo iptables -A LOGGING-Allowed -j ACCEPT"
+        ssh elkadmin@$server "sudo iptables -N LOGGING-Dropped"
+        ssh elkadmin@$server "sudo iptables -A LOGGING-Dropped -m limit --limit 2/min -j LOG --log-prefix "IPTables-Dropped: " --log-level 4"
+        ssh elkadmin@$server "sudo iptables -A LOGGING-Dropped -j REJECT --reject-with icmp-proto-unreachable"
 
         # Allow all established and related connections
         echo "Allowing all Established,Related connections on server: " $server
-        ssh root@$server "iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j LOGGING-Allowed"
+        ssh elkadmin@$server "sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j LOGGING-Allowed"
 
         # Allow all connections from localhost to localhost
         echo "Allowing all connections from localhost on server:" $server
-        ssh root@$server "iptables -A INPUT -i lo -j LOGGING-Allowed"
+        ssh elkadmin@$server "sudo iptables -A INPUT -i lo -j LOGGING-Allowed"
 
         # Drop INVALID headers and checksums, invalid TCP flags, invalid ICMP messages and out of sequence packets
         echo "Dropping INVALID headers and checksums, invalid TCP flags, invalid ICMP messages and out of sequence packets"
-        ssh root@$server "iptables -A INPUT -m conntrack --ctstate INVALID -j DROP"
+        ssh elkadmin@$server "sudo iptables -A INPUT -m conntrack --ctstate INVALID -j DROP"
 
         # Allow ICMP for everything
         echo "Allowing ICMP Firewall rules on: " $server
-        ssh root@$server "iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j LOGGING-Allowed"
+        ssh elkadmin@$server "sudo iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j LOGGING-Allowed"
 
         # Setup default rules not related to ELK
         echo "Applying all default non-ELK related ports"
         for defaultportallow in $(cat firewall_default_tcp_ports_allowed.txt); do
                 echo "Allowing TCP port: " $defaultportallow "On Server: " $server
-                ssh root@$server "iptables -A INPUT -p tcp --dport $defaultportallow -j DEFAULT-Allowed"
-                ssh root@$server "iptables -A DEFAULT-Allowed -p tcp --dport $defaultportallow -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                ssh elkadmin@$server "sudo iptables -A INPUT -p tcp --dport $defaultportallow -j DEFAULT-Allowed"
+                ssh elkadmin@$server "sudo iptables -A DEFAULT-Allowed -p tcp --dport $defaultportallow -m conntrack --ctstate NEW -j LOGGING-Allowed"
         done
         for defaultportallow in $(cat firewall_default_udp_ports_allowed.txt); do
                 echo "Allowing UDP port: " $defaultportallow "On Server: " $server
-                ssh root@$server "iptables -A INPUT -p udp --dport $defaultportallow -j DEFAULT-Allowed"
-                ssh root@$server "iptables -A DEFAULT-Allowed -p udp --dport $defaultportallow -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                ssh elkadmin@$server "sudo iptables -A INPUT -p udp --dport $defaultportallow -j DEFAULT-Allowed"
+                ssh elkadmin@$server "sudo iptables -A DEFAULT-Allowed -p udp --dport $defaultportallow -m conntrack --ctstate NEW -j LOGGING-Allowed"
         done
 done
 
@@ -72,14 +72,14 @@ for server in $(cat elk-nodes.txt); do
         for portallow in $(cat firewall_unrestricted_tcp_ports_allowed.txt); do
                 echo "Allowing TCP port: " $portallow "On server: " $server
                 # Setup first needed rule to send rule to chain for each protocol
-                ssh root@$server "iptables -A INPUT -p tcp --dport $portallow -j ELK-RELATED-Allowed"
-                ssh root@$server "iptables -A ELK-RELATED-Allowed -p tcp --dport $portallow -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                ssh elkadmin@$server "sudo iptables -A INPUT -p tcp --dport $portallow -j ELK-RELATED-Allowed"
+                ssh elkadmin@$server "sudo iptables -A ELK-RELATED-Allowed -p tcp --dport $portallow -m conntrack --ctstate NEW -j LOGGING-Allowed"
         done
         for portallow in $(cat firewall_unrestricted_udp_ports_allowed.txt); do
                 echo "Allowing UDP port: " $portallow "On server: " $server
                 # Setup first needed rule to send rule to chain for each protocol
-                ssh root@$server "iptables -A INPUT -p udp --dport $portallow -j ELK-RELATED-Allowed"
-                ssh root@$server "iptables -A ELK-RELATED-Allowed -p udp --dport $portallow -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                ssh elkadmin@$server "sudo iptables -A INPUT -p udp --dport $portallow -j ELK-RELATED-Allowed"
+                ssh elkadmin@$server "sudo iptables -A ELK-RELATED-Allowed -p udp --dport $portallow -m conntrack --ctstate NEW -j LOGGING-Allowed"
         done
 done
 
@@ -89,41 +89,41 @@ for server in $(cat elk-nodes.txt); do
         echo "Applying all ELK related firewall rules on: " $server
         for portallow in $(cat firewall_restricted_tcp_ports_allowed.txt); do
                 # allowing other logstash nodes access
-                ssh root@$server "iptables -A INPUT -p tcp --dport $portallow -j ELK-RELATED-Allowed"
+                ssh elkadmin@$server "sudo iptables -A INPUT -p tcp --dport $portallow -j ELK-RELATED-Allowed"
                 for node in $(cat elk-nodes.txt); do
                         echo "Allowing TCP port: " $portallow "From ELK node: " $node "On server: " $server
                         # Setup first needed rule to send rule to chain for each protocol
-                        ssh root@$server "iptables -A ELK-RELATED-Allowed -p tcp --dport $portallow -s $node -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                        ssh elkadmin@$server "sudo iptables -A ELK-RELATED-Allowed -p tcp --dport $portallow -s $node -m conntrack --ctstate NEW -j LOGGING-Allowed"
                 done
                 # allowing haproxy nodes access
                 for node in $(cat elk-haproxy-nodes.txt); do
                         echo "Allowing TCP port: " $portallow "From haproxy node: " $node "On server: " $server
-                        ssh root@$server "iptables -A ELK-RELATED-Allowed -p tcp --dport $portallow -s $node -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                        ssh elkadmin@$server "sudo iptables -A ELK-RELATED-Allowed -p tcp --dport $portallow -s $node -m conntrack --ctstate NEW -j LOGGING-Allowed"
                 done
                 # allowing admin user stations access
                 for station in $(cat admin_stations.txt); do
                         echo "Allowing TCP port: " $portallow "From admin station: " $station "On server: " $server
-                        ssh root@$server "iptables -A ELK-RELATED-Allowed -p tcp --dport $portallow -s $station -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                        ssh elkadmin@$server "sudo iptables -A ELK-RELATED-Allowed -p tcp --dport $portallow -s $station -m conntrack --ctstate NEW -j LOGGING-Allowed"
                 done
         done
         # Apply UDP Ports
         for portallow in $(cat firewall_restricted_udp_ports_allowed.txt); do
                 # allowing other ELK nodes access
-                ssh root@$server "iptables -A INPUT -p udp --dport $portallow -j ELK-RELATED-Allowed"
+                ssh elkadmin@$server "sudo iptables -A INPUT -p udp --dport $portallow -j ELK-RELATED-Allowed"
                 for node in $(cat elk-nodes.txt); do
                         echo "Allowing UDP port: " $portallow "From ELK node: " $node "On server: " $server
                         # Setup first needed rule to send rule to chain for each protocol
-                        ssh root@$server "iptables -A ELK-RELATED-Allowed -p udp --dport $portallow -s $node -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                        ssh elkadmin@$server "sudo iptables -A ELK-RELATED-Allowed -p udp --dport $portallow -s $node -m conntrack --ctstate NEW -j LOGGING-Allowed"
                 done
                 # allowing haproxy nodes access
                 for node in $(cat elk-haproxy-nodes.txt); do
                         echo "Allowing UDP port: " $portallow "From haproxy node: " $node "On server: " $server
-                        ssh root@$server "iptables -A ELK-RELATED-Allowed -p udp --dport $portallow -s $node -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                        ssh elkadmin@$server "sudo iptables -A ELK-RELATED-Allowed -p udp --dport $portallow -s $node -m conntrack --ctstate NEW -j LOGGING-Allowed"
                 done
                 # allowing admin user stations access
                 for station in $(cat admin_stations.txt); do
                         echo "Allowing UDP port: " $portallow "From admin station: " $station "On server: " $server
-                        ssh root@$server "iptables -A ELK-RELATED-Allowed -p udp --dport $portallow -s $station -m conntrack --ctstate NEW -j LOGGING-Allowed"
+                        ssh elkadmin@$server "sudo iptables -A ELK-RELATED-Allowed -p udp --dport $portallow -s $station -m conntrack --ctstate NEW -j LOGGING-Allowed"
                 done
         done
 done
@@ -131,16 +131,16 @@ done
 # Apply deny firewall rules for everything else other than allowed above
 for server in $(cat elk-nodes.txt); do
         echo "Applying Firewall drop rules on: " $server
-        ssh root@$server "iptables -A INPUT -j LOGGING-Dropped"
-        ssh root@$server "iptables -A FORWARD -j LOGGING-Dropped"
+        ssh elkadmin@$server "sudo iptables -A INPUT -j LOGGING-Dropped"
+        ssh elkadmin@$server "sudo iptables -A FORWARD -j LOGGING-Dropped"
 done
 
 # Apply Default Filtering Policy
 for server in $(cat elk-nodes.txt); do
         echo "Setting Default Firewall policy to drop on: " $server
-        ssh root@$server "iptables -P INPUT DROP"
-        ssh root@$server "iptables -P FORWARD DROP"
-        ssh root@$server "iptables -P OUTPUT ACCEPT"
+        ssh elkadmin@$server "sudo iptables -P INPUT DROP"
+        ssh elkadmin@$server "sudo iptables -P FORWARD DROP"
+        ssh elkadmin@$server "sudo iptables -P OUTPUT ACCEPT"
 done
 
 # Save Firewall rules
