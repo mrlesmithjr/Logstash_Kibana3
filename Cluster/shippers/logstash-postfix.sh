93c7f21b5deab9a1d2c469bdd504cc3994fa1b94
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-07-03 16:56:15 -0400

    Create logstash-postfix.sh

---
+++ Cluster/shippers/logstash-postfix.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+#Provided by @mrlesmithjr
+#EveryThingShouldBeVirtual.com
+
+# This script sets up logstash as a shipper for postfix sending to redis on backend logstash processors
+
+set -e
+# Setup logging
+# Logs stderr and stdout to separate files.
+exec 2> >(tee "./logstash-postfix.err")
+exec > >(tee "./logstash-postfix.log")
+
+# Setting colors for output
+red="$(tput setaf 1)"
+yellow="$(tput bold ; tput setaf 3)"
+NC="$(tput sgr0)"
+
+# Capture your FQDN Domain Name and IP Address
+echo "${yellow}Capturing your hostname${NC}"
+yourhostname=$(hostname)
+echo "${yellow}Capturing your domain name${NC}"
+yourdomainname=$(dnsdomainname)
+echo "${yellow}Capturing your FQDN${NC}"
+yourfqdn=$(hostname -f)
+echo "${yellow}Detecting IP Address${NC}"
+IPADDY="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+echo "Your hostname is currently ${red}$yourhostname${NC}"
+echo "Your domain name is currently ${red}$yourdomainname${NC}"
+echo "Your FQDN is currently ${red}$yourfqdn${NC}"
+echo "Detected IP Address is ${red}$IPADDY${NC}"
+
+# Disable CD Sources in /etc/apt/sources.list
+echo "Disabling CD Sources and Updating Apt Packages and Installing Pre-Reqs"
+sed -i -e 's|deb cdrom:|# deb cdrom:|' /etc/apt/sources.list
+apt-get -qq update
+
+############################### Logstash - Shipper Setup ##################################
+# Install Oracle Java 7 **NOT Used - Installing openjdk-7-jre above
+echo "Installing Oracle Java 7"
+add-apt-repository -y ppa:webupd8team/java
+apt-get -qq update
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get install -y oracle-java7-installer oracle-java7-set-default
+
+# Install Logstash
+cd /opt
+#wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.1.tar.gz
+wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
+tar zxvf logstash-*.tar.gz
+mv logstash-1.4.*/ logstash
+
+# Create Logstash Init Script
+(
+cat <<'EOF'
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides:          logstash
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
+. /lib/lsb/init-functions
+
+name="logstash"
+logstash_bin="/opt/logstash/bin/logstash"
+logstash_conf="/etc/logstash/logstash.conf"
+logstash_log="/var/log/logstash.log"
+pid_file="/var/run/$name.pid"
+patterns_path="/etc/logstash/patterns"
+
+start () {
+        command="${logstash_bin} -- agent -f $logstash_conf --log ${logstash_log}"
+
+        log_daemon_msg "Starting $name" "$name"
+        if start-stop-daemon --start --quiet --oknodo --pidfile "$pid_file" -b -m --exec $command; then
+                log_end_msg 0
+        else
+                log_end_msg 1
+        fi
+}
+
+stop () {
+        log_daemon_msg "Stopping $name" "$name"
+        start-stop-daemon --stop --quiet --oknodo --pidfile "$pid_file"
+}
+
+status () {
+        status_of_proc -p "$pid_file" "$name"
+}
+
+case $1 in
+        start)
+                if status; then exit 0; fi
+                start
+                ;;
+        stop)
+                stop
+                ;;
+        reload)
+                stop
+                start
+                ;;
+        restart)
+                stop
+                start
+                ;;
+        status)
+                status && exit 0 || exit $?
+                ;;
+        *)
+                echo "Usage: $0 {start|stop|restart|reload|status}"
+                exit 1
+                ;;
+esac
+
+exit 0
+EOF
+) | tee /etc/init.d/logstash
+
+# Make logstash executable
+chmod +x /etc/init.d/logstash
+
+# Enable logstash start on bootup
+update-rc.d logstash defaults 96 04
+
+# Collect information for VIP (IP|HOSTNAME) To inject into script
+echo "Enter the HAProxy VIP (IP|HOSTNAME) for your setup"
+echo "This will be what points to your back-end ELK Cluster Client Nodes"
+echo "This will also be the VIP, hostname or FQDN that was setup in haproxy.cfg"
+echo "(Example - 10.0.101.60, logstash or logstash.example.local"
+echo -n "Enter your information here: "
+read logstashinfo
+echo "You entered ${red}$logstashinfo${NC}"
+
+# Create Logstash configuration file
+mkdir /etc/logstash
+tee -a /etc/logstash/logstash.conf <<EOF
+input {
+        file {
+                path => "/var/log/mail.log"
+                type => "postfix-log"
+                sincedb_path => "/var/log/.postfixlogsincedb"
+        }
+}
+output {
+        redis {
+                host => $logstashinfo
+                data_type => "list"
+                key => "logstash"
+        }
+}
+
+# Restart logstash service
+service logstash restart
+
+# Logrotate job for logstash
+tee -a /etc/logrotate.d/logstash <<EOF
+/var/log/logstash.log {
+        monthly
+        rotate 12
+        compress
+        delaycompress
+        missingok
+        notifempty
+        create 644 root root
+}
+EOF
+
+# All Done
+echo "Installation has completed!!"
+echo "${yellow}EveryThingShouldBeVirtual.com${NC}"
+echo "${yellow}@mrlesmithjr${NC}"
+echo "${yellow}Enjoy!!!${NC}"
