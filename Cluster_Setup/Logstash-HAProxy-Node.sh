bf98e716214d4ef83bd3a672c122949bb263587c
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-10-14 14:43:12 -0400

    Updated Logstash to 1.4.2

--- Cluster_Setup/Logstash-HAProxy-Node.sh
+++ Cluster_Setup/Logstash-HAProxy-Node.sh
@@ -51,7 +51,8 @@ apt-get install -y --force-yes git curl nginx software-properties-common
  
 # Install Logstash
 cd /opt
-wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.1.tar.gz
+# wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.1.tar.gz
+wget https://download.elasticsearch.org/logstash/logstash/logstash-contrib-1.4.2.tar.gz
 tar zxvf logstash-*.tar.gz
 mv logstash-1.4.1 logstash
 /opt/logstash/bin/plugin install contrib
