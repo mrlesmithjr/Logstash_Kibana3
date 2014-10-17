4b69af776a8089dbfc947b97373ef1dd051bcd1b
Larry Smith Jr. <larry.e.smith.jr@gmail.com>
2014-10-17 15:10:47 -0400

    Update Logstash-ELK-ES-Cluster-broker-node.sh

--- Cluster_Setup/Logstash-ELK-ES-Cluster-broker-node.sh
+++ Cluster_Setup/Logstash-ELK-ES-Cluster-broker-node.sh
@@ -52,10 +52,10 @@ apt-get install -y --force-yes ruby ruby1.9.1-dev libcurl4-openssl-dev
 
 # Install Logstash
 cd /opt
-wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.1.tar.gz
-wget wget https://download.elasticsearch.org/logstash/logstash/logstash-contrib-1.4.2.tar.gz
+wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
 tar zxvf logstash-*.tar.gz
-mv logstash-1.4.1 logstash
+rm logstash-*.tar.gz
+mv logstash-1.4.*/ logstash
 /opt/logstash/bin/plugin install contrib
 
 # Create Logstash Init Script
